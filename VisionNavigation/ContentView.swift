cc27dd79b173d0c90340c74d406541d7508bd54b
uzakotim <timauzakov@gmail.com>
2025-12-03 15:04:33 +0500

    Initial Commit

---
+++ VisionNavigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisionNavigation
+//
+//  Created by Timur Uzakov on 03/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisionNavigation/VisionNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionNavigationApp.swift
+//  VisionNavigation
+//
+//  Created by Timur Uzakov on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
